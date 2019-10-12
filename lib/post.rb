df7cde9519dcea85d5d3bd5f66c352a73ba0c01d
Amgad Malik <amgadmalik@yahoo.com>
2019-10-12 02:28:31 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,10 @@
 class Post 
-  attr_accessor :author, :name, :genre 
+  attr_accessor :author, :name
   
   @@all = []
   
   def initialize(name, genre)
     @name = name 
-    @genre = genre
     save 
   end
   
