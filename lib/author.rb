b78f54fbb20699357138d05777b556af511e72d8
Amgad Malik <amgadmalik@yahoo.com>
2019-10-12 02:33:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,11 +6,14 @@ class Author
   end 
   
   def add_post(pos)
-    post.author = self 
+    pos.author = self 
   end 
   
   def posts 
     Post.all.select {|pos| pos.author == self}
+  end 
+  def songs 
+    Song.all.select {|song| song.artist == self}
   end 
   
   def add_post_by_name(name)
